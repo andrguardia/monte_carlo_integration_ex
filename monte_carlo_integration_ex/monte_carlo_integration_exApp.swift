638136ff1445c6e4a9765708280925c9717aa2a0
Andre Guardia <andre.guardiaq@gmail.com>
2023-02-03 16:36:47 -0600

    Initial Commit

---
+++ monte_carlo_integration_ex/monte_carlo_integration_exApp.swift
@@ -0,0 +1,17 @@
+//
+//  monte_carlo_integration_exApp.swift
+//  monte_carlo_integration_ex
+//
+//  Created by IIT Phys 440 on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct monte_carlo_integration_exApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
